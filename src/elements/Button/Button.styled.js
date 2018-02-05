9aa4312aadd860b7513eb7e0d767584a46a74fca
mino <mino@mino.local>
2018-02-05 21:14:33 +0900

    [추가] animation 예제 추가

--- src/elements/Button/Button.styled.js
+++ src/elements/Button/Button.styled.js
@@ -1,11 +1,23 @@
 /* External dependencies */
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const Button = styled.button`
   width: 80px;
   height: 80px;
   background-color: ${props => props.bgColor || 'red'};
   color: white;
+  opacity: 0.3;
+  animation: ${rotate360} 2s linear infinite;
 `;
 
 export default Button;
