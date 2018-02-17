8a821a2796cb56007453819ff64703c7990d3767
mino <mino@mino.local>
2018-02-18 01:06:04 +0900

    [추가] reFetch에 delete 메서드 추가 & signOut action 추가

--- src/api/managerAPI.js
+++ src/api/managerAPI.js
@@ -6,3 +6,5 @@ export const getMe = () => reFetch.get('/managers/me');
 export const signIn = manager => reFetch.post('/managers/signin', manager);
 
 export const signUp = manager => reFetch.post('/managers/signup', manager);
+
+export const signOut = () => reFetch.delet('/managers/signout');
