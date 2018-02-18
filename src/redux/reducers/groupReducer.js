cb8e29051df0b9e3411015965e5e6fed7a864724
mino <mino@mino.local>
2018-02-18 17:57:24 +0900

    [추가] create group action에 group, session reducer 대응 추가

--- src/redux/reducers/groupReducer.js
+++ src/redux/reducers/groupReducer.js
@@ -10,6 +10,7 @@ export default (state = initState, action) => {
   switch (action.type) {
     case AT.REQUEST_GET_ME_SUCCESS:
     case AT.REQUEST_SIGN_IN_SUCCESS:
+    case AT.REQUEST_CREATE_GROUP_SUCCESS:
       return {
         ...state,
         group: new Group(action.payload.group),
