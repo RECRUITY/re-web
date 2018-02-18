0068baac2734d3eec07a9ebbbe816bd91b3e436d
mino <mino@mino.local>
2018-02-18 17:10:56 +0900

    [추가] session, group이 reducer에 저장 안되는 버그 수정

--- src/redux/reducers/sessionReducer.js
+++ src/redux/reducers/sessionReducer.js
@@ -8,8 +8,8 @@ const initState = {
 
 export default (state = initState, action) => {
   switch (action.type) {
-    case AT.REQUEST_GET_ME:
-    case AT.REQUEST_SIGN_IN:
+    case AT.REQUEST_GET_ME_SUCCESS:
+    case AT.REQUEST_SIGN_IN_SUCCESS:
       return {
         ...state,
         session: new Session(action.payload.session),
